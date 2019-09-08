161cd6fec18418f441ca5132e9f8da7893649333
M0r13n <31622033+M0r13n@users.noreply.github.com>
2019-09-08 13:53:36 +0200

    Update credit.c

--- cs50/pset1/credit.c
+++ cs50/pset1/credit.c
@@ -1,63 +1,105 @@
-#include <cs50.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
+#include <cs50.h>
 
-void check(long n)
+int splitter(long cardnumber, int position)
 {
-    int cur;
-    int prev;
-    int l_count = 0;
-    int sum = 0;
-    int d_digit;
+    long num1 = pow(10, position+1);
+    long num2 = pow(10, position);
+    int digit = (cardnumber % num1 / num2);
+    return digit;
+}
 
-    // Iterate over every decimal value Step-By-Step
-    while (n)
+int givelength(long number)
+{
+    if (number >= 1000000000000000)
     {
-        prev = cur;
-        cur = n % 10;
-
-        if (l_count % 2)
-        {
-            d_digit = 2 * cur;
-            sum += d_digit % 10;
-            if (d_digit >= 10)
-                sum += d_digit / 10;
-        }
-        else
-        {
-            sum += cur;
-        }
-        n /= 10;
-        l_count++;
+        return 16;
     }
-
-    // check if the credit card is valid at all
-    if (sum % 10 == 0)
+    if (number >= 100000000000000)
     {
-        if (cur == 4 && (l_count == 13 || l_count == 16))
-        {
-            printf("VISA\n");
-        }
-        else if (cur == 3 && (prev == 4 || prev == 7) && l_count == 15)
-        {
-            printf("AMEX\n");
-        }
-        else if (cur == 5 && prev >= 1 && prev <= 5 && l_count == 16)
-        {
-            printf("MASTERCARD\n");
-        }
-        else
-        {
-            printf("INVALID\n");
-        }
-        return;
+        return 15;
     }
-    printf("INVALID\n");
+    if (number >= 10000000000000)
+    {
+        return 14;
+    }
+    if (number >= 1000000000000)
+    {
+        return 13;
+    }
+    // du musst nen default wert zurückgeben, sonst zickt z.B. GCC herum
+    return -1;
 }
 
 int main(void)
 {
-    long n = get_long("Number: ");
-    check(n);
-    exit(0);
+    long cardnumber = get_long("Number: ");
+
+
+    if ( givelength(cardnumber) > 16 || givelength(cardnumber) < 13)
+    {
+        printf("INVALID\n");
+        return 0;
+    }
+    else
+    {
+        int firstSum = 0;
+
+        for (int x = 1; x < givelength(cardnumber); x += 2)
+        {
+            int d = splitter(cardnumber, x) * 2;
+            if(d > 9)
+            {
+                int d1 = splitter(d, 1);
+                firstSum += d1;
+                int d2 = splitter(d, 0);
+                firstSum += d2;
+            }
+            else
+            {
+                firstSum += d;
+            }
+        }
+
+        for (int x = 0; x < (givelength(cardnumber) + 1); x += 2)
+        {
+            int d = splitter(cardnumber, x);
+            firstSum += d;
+        }
+
+        if (firstSum % 10 == 0)
+        {
+            // Ein long hat 64 bit und ein int 32.
+            // Wenn du einen long auf einen int assignst,
+            // passieren krumme Dinge, idr. negative werte
+            long numX = cardnumber;
+
+            // Idee ist richtig, aber du kennst die Länge der Zahl nicht
+            // Die kann 15, 16, etc Ziffern haben
+            // bei der Amex ratterst du zum Beispiel über das Ziel hinaus
+            while (numX >= 100)
+            {
+                numX /= 100;
+            }
+
+            printf("%li\n", numX);
+            if (numX < 56 && numX > 50)
+            {
+                printf("MasterCard\n");
+                return 0;
+            }
+            if (numX == 34 || numX == 37)
+            {
+                printf("American Express\n");
+                return 0;
+            }
+            if (numX < 50 && numX > 39)
+            {
+                printf("VISA\n");
+                return 0;
+            }
+        }
+    }
 }
+
